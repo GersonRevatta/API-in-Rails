1815e98927dbedc27b3ec238374398b65c9b9811
gerson <gersonrevatta@gmail.com>
2017-08-31 17:10:10 -0500

    render json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :create, :destroy, :update, :show]
+
+  namespace :api, defaults: {format: 'json'} do
+    resources :users
+  end
+  resources :users  
+  root "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
